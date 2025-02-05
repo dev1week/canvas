4edb2c5148c33d467ff8d182199d0ce3b3a8b848
dev1week <rondo2860@gmail.com>
2025-02-05 17:13:50 +0900

    feat: 싱글턴 적용-3 #1

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,4 @@
-
-let instance: Grimpan; 
-
-class Grimpan{
-    constructor(canvas: HTMLElement|null){
-        if(!canvas||!(canvas instanceof HTMLCanvasElement)){
-            throw new Error("canvas 엘리먼트를 입력하세요");
-        }
-
-        if(!instance){
-            instance = this;
-        }
-        return instance; 
-    }
-    initialize(){}
-    initializeMenu(){}
-}
-const g1 = new Grimpan(document.querySelector('#canvas'));
-const g2 = new Grimpan(document.querySelector('#canvas'));
+import g1 from './grimpan.js'
+import g2 from './grimpan.js'
 
 console.log(g1===g2);
